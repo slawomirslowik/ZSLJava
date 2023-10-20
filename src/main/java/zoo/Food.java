3141de487cda69bd08c6335376eaee546819ce04
unknown <Slawomir Slowik>
2023-10-20 08:37:43 +0200

    added description

--- src/main/java/zoo/Food.java
+++ src/main/java/zoo/Food.java
@@ -4,6 +4,9 @@ public class Food {
     String foodType;
     long calories;
 
+    //konstruktor - wywoływane gdy użyjemy słowa\
+    //kluczowego 'new'
+    // left alt + insert
     public Food(String foodType, long calories) {
         this.foodType = foodType;
         this.calories = calories;
