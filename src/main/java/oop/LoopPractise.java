072c52a5570aeb854131ebe12f4f840dfaa8baa9
Slawomir Slowik <slawomirslowik@yahoo.pl>
2023-11-09 09:00:35 +0100

    printSquare

---
+++ src/main/java/oop/LoopPractise.java
@@ -0,0 +1,22 @@
+package oop;
+
+import java.sql.SQLOutput;
+
+public class LoopPractise {
+
+    public static void main(String[] args) {
+
+        printSquare(7);
+    }
+
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int y = 0; y < size * 3; y++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+    }
+
+}
